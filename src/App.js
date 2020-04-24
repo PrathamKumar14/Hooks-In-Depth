fee2ba6c8f250ed69d8dee03dec9bd8b6cfd95aa
Pratham <prathamsolera@gmail.com>
2020-04-24 18:21:34 +0530

    Add useReducer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import UseState from "./components/Basic-Hooks/UseState";
 import UseEffect from "./components/Basic-Hooks/UseEffect";
 import UseContext from "./components/Basic-Hooks/UseContext";
+import UseRef from "./components/Additional-Hooks/UseRef";
+import UseReducer from "./components/Additional-Hooks/UseReducer";
 import "./styles.css";
 
 export default function App() {
@@ -11,12 +13,20 @@ export default function App() {
         <u>Hooks In depth</u>
       </h1>
       <hr />
-      <h2>useState Example</h2>
+      <h2 className="example">useState Example</h2>
       <UseState />
       <hr />
+      <h2 className="example">useEffect Example</h2>
       <UseEffect />
       <hr />
+      <h2 className="example">useContext Example</h2>
       <UseContext />
+      <hr />
+      <h2 className="example">useRef Example</h2>
+      <UseRef />
+      <hr />
+      <h2 className="example">useReducer Example</h2>
+      <UseReducer />
       <hr />
     </div>
   );
